e0877f8405057ab6eaad1d186d7459de4a5caf75
virendrarajpurohit <37048535+virendrarajpurohit@users.noreply.github.com>
2022-03-26 16:50:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then(response => response.json())
+      .then(data => {
+        setContacts(data.results);
+      });
+  }, []);
+  return (
+    <>
+        {
+          contacts.map(contact => (
+            <ContactCard
+                avatar={contact.picture.large}
+                name={contact.name.first + " " + contact.name.last}
+                email={contact.email}
+                age={contact.dob.age}
+              />
+          ))
+        }
+    </>
+  );
+}
+
+const ContactCard = (props) => {
+  const [showAge, setShowAge] = useState(true);
+  return (
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div>
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>Toggle Age</button>
+        {showAge && <p>Age: {props.age}</p>}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
